db56da8a1f29b08a5ff566b72afa7fdedeb5c3f6
p-freire <pauloguilhermefreire@gmail.com>
2016-11-10 10:03:45 -0200

    Add files via upload

---
+++ Lista2/ex3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, *p, m, contador, i;
+
+    printf("Entre com o numero de pessoas: ");
+    scanf("%d", &n);
+    printf("Entre com o salto: ");
+    scanf("%d", &m);
+
+    // Aloca o vetor de tamanho n
+    p = malloc(n * sizeof(int));
+
+    if(p == NULL)
+        return -1;
+
+    // Preenche o vetor com numeros sequenciais de 1 a N
+    for(i = 0; i < n; ++i)
+        p[i] = i + 1;
+
+    contador = n; // conta o numero de pessoas ainda vivas
+    i = 0;
+    int aux = 0; // auxiliar pra contar quantas pessoas ja passaram
+    while(contador > 1)
+    {
+        while(aux < m)
+        {
+            if(p[i] != -1)
+                ++aux;
+            if(aux < m)
+                i = (i + 1) % n;
+        }
+        p[i] = -1;
+        --contador;
+        aux = 0;
+    }
+
+    // Acha o sobrevivente
+    for(i = 0; i < n; ++i)
+        if(p[i] != -1)
+            printf("O sobrevivente foi: %d\n", p[i]);
+
+    free(p);
+    return 0;
+}
---
+++ Lista2/ex4.c
@@ -0,0 +1,22 @@
+/*
+	Esse problema é conhecido com o problema de Josephus
+    Usando recursão, o problema se torna O(n)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int josephus(int n, int k)
+{
+    return n == 1 ? 1 : ((josephus(n - 1, k) + k - 1) % n) + 1;
+}
+
+int main()
+{
+    int n, k;
+    printf("Entre com o numero de pessoas e o salto: ");
+    scanf("%d %d", &n, &k);
+    printf("Sobrevivente: %d\n", josephus(n, k));
+
+    return 0;
+}
