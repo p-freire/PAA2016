449d7665e495996d9060ab5798e9e623e944d54e
p-freire <pauloguilhermefreire@gmail.com>
2016-12-01 09:55:17 -0200

    Update pico.cpp

--- misc/pico.cpp
+++ misc/pico.cpp
@@ -1,24 +1,33 @@
-/*
- Definição: um elemento pico de um vetor é aquele que é maior que seus dois vizinhos.
- Exemplo: no vetor {10, 20, 15, 2, 23, 90, 67} há dois elementos pico, o 20 e o 90.
- Faça um programa que retorne pelo menos um dos elementos pico de um dado vetor,
- primeiramente usando força bruta e posteriormente usando divisão e conquista.
- Note que você só precisa retornar um elemento pico, os vetores tem pelo menos um elemento pico e não há repetições.
-*/
-
 #include <stdio.h>
 #include <limits.h>
 #include <algorithm>
 #include <vector>
 
-int pico(std::vector<int> v, int l, int r)
+// Complexidade O(n)
+int picoLinear(std::vector<int> v, int l, int r)
 {
     if(r - l == 0)
         return v[l] > v[l - 1] && v[l] > v[l + 1] ? v[l] : INT_MIN;
 
     int m = l + (r - l) / 2;
 
-    return std::max(pico(v, l, m), pico(v, m + 1, r));
+    return std::max(picoLinear(v, l, m), picoLinear(v, m + 1, r));
+}
+
+// Complexidade O(log n)
+int picoLog(std::vector<int> v, int l, int r)
+{
+    if(l <= r)
+    {
+        int m = l + (r - l) / 2;
+        if(v[m] > v[m - 1] && v[m] > v[m + 1])
+            return v[m];
+        else if(v[m] < v[m - 1])
+            return picoLog(v, l, m - 1);
+        else
+            return picoLog(v, m + 1, r);
+    }
+    return INT_MIN;
 }
 
 
@@ -38,7 +47,7 @@ int main()
         for(int i = 0; i < n; ++i)
             scanf("%d", &v[i]);
 
-        int res = pico(v, 1, n - 2);
+        int res = picoLog(v, 1, n - 2);
 
         if(res != INT_MIN)
             printf("Pico: %d.\n", res);
